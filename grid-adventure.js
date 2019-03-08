97f210f7a6c42d57165885d9ace43b2d48200419
Alex Beernink <beankernelxi@gmail.com>
2019-03-08 10:05:01 -0800

    mostly refactoring

--- grid-adventure.js
+++ grid-adventure.js
@@ -1,12 +1,20 @@
-var canvas = document.querySelector('canvas');
-var cc = canvas.getContext('2d');
 
-canvas.width = 500;
-canvas.height = 500;
+//////////  Canvas  ////////////////////////////////////////////////////////////
+
+var canvas = document.querySelector('canvas');
+var context = canvas.getContext('2d');
+
+var buffer = document.createElement('canvas')
+var cc = buffer.getContext('2d')
+
+canvas.width = buffer.width = 500;
+canvas.height = buffer.height = 500;
 
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;    // for now assume it never resizes
 const canvasCenter = {x:canvasWidth/2,y:canvasHeight/2};
+
+//////////  Colors!  ///////////////////////////////////////////////////////////
 
 const SimpleColor = {
   red: '#e6194b', green: '#3cb44b', yellow: '#ffe119', blue: '#4363d8', orange: '#f58231', purple: '#911eb4',
@@ -15,19 +23,14 @@ const SimpleColor = {
   navy: '#000075', grey: '#a9a9a9', white: '#ffffff', black: '#000000',
 }; // https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
 
-const defaultSquare = {color: SimpleColor.cyan, number: 1, active: false}
+//////////  World  /////////////////////////////////////////////////////////////
 
+const defaultSquare = {color: SimpleColor.cyan, number: 1, active: false}
 const worldWidth = 1000
 const worldArray = new Array(worldWidth*worldWidth).fill(defaultSquare);
 function world(x,y){return worldArray[worldWidth*y + x];}
-worldArray[worldWidth*505 + 502] = {color: SimpleColor.blue, number: 0, active: true};
-worldArray[worldWidth*505 + 503] = {color: SimpleColor.blue, number: 1, active: false};
-worldArray[worldWidth*505 + 504] = {color: SimpleColor.blue, number: 2, active: false};
-worldArray[worldWidth*505 + 505] = {color: SimpleColor.blue, number: 3, active: false};
-worldArray[worldWidth*506 + 502] = {color: SimpleColor.blue, number: 4, active: false};
-worldArray[worldWidth*506 + 503] = {color: SimpleColor.blue, number: 5, active: false};
-worldArray[worldWidth*506 + 504] = {color: SimpleColor.blue, number: 6, active: false};
-worldArray[worldWidth*506 + 505] = {color: SimpleColor.blue, number: 7, active: false};
+
+//////////  Globals  ///////////////////////////////////////////////////////////
 
 const settings = {
   textOn: true,
@@ -35,20 +38,51 @@ const settings = {
 };
 
 const global = {
-  scale: {x:60,y:60}, // TODO: initialize based on canvas size
-  pos: {x:500,y:500},
-  offset: {x:63,y:63},
-  moving: false,
-  lastClick: {x:0,y:0},
+  scale: {x:60,y:60}, // width and height of each cell
+  pos: {x:500,y:500}, // index into world array
+  offset: {x:43,y:43}, // distance from corner of canvas to corner of top-left cell
+  moving: false, // save on updates
+  lastClick: {x:0,y:0}, // as a pos ^
 };
+
+//////////  Other  /////////////////////////////////////////////////////////////
 
 // used by drawPips
 const whichPip = { 0:[], 1:[4], 2:[2,6], 3:[2,4,6], 4:[0,2,6,8], 5:[0,2,4,6,8], 6:[0,2,3,5,6,8], 7:[0,2,3,4,5,6,8], 8:[0,1,2,3,5,6,7,8] };
 
-const drawMargin = 1;
+const drawMargin = 1; // difference between scale and tile (gap will be background color by default)
+
+
+//* Only temporary ;) Will be moved to a world file asap
+worldArray[worldWidth*505 + 502] = {color: SimpleColor.blue, number: 0, active: true};
+worldArray[worldWidth*505 + 503] = {color: SimpleColor.blue, number: 1, active: false};
+worldArray[worldWidth*505 + 504] = {color: SimpleColor.blue, number: 2, active: false};
+worldArray[worldWidth*506 + 502] = {color: SimpleColor.blue, number: 3, active: false};
+worldArray[worldWidth*506 + 503] = {color: SimpleColor.blue, number: 4, active: false};
+worldArray[worldWidth*506 + 504] = {color: SimpleColor.blue, number: 5, active: false};
+worldArray[worldWidth*507 + 502] = {color: SimpleColor.blue, number: 6, active: false};
+worldArray[worldWidth*507 + 503] = {color: SimpleColor.blue, number: 7, active: false};
+worldArray[worldWidth*507 + 504] = {color: SimpleColor.blue, number: 8, active: false};
+
+worldArray[worldWidth*505 + 507] = {color: SimpleColor.blue, number: 0, active: true};
+worldArray[worldWidth*505 + 509] = {color: SimpleColor.blue, number: 1, active: false};
+worldArray[worldWidth*505 + 511] = {color: SimpleColor.blue, number: 2, active: false};
+worldArray[worldWidth*507 + 507] = {color: SimpleColor.blue, number: 3, active: false};
+worldArray[worldWidth*507 + 509] = {color: SimpleColor.blue, number: 4, active: false};
+worldArray[worldWidth*507 + 511] = {color: SimpleColor.blue, number: 5, active: false};
+worldArray[worldWidth*509 + 507] = {color: SimpleColor.blue, number: 6, active: false};
+worldArray[worldWidth*509 + 509] = {color: SimpleColor.blue, number: 7, active: false};
+worldArray[worldWidth*509 + 511] = {color: SimpleColor.blue, number: 8, active: false};
+//*/
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+//////////  Top Level  /////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 function start(){
-  const updateRate = 30; // ups
+  const updateRate = 30; // per second
   window.setInterval(update, 1000/updateRate);
   update();
   draw();
@@ -59,15 +93,18 @@ function update() {
   if(global.moving) {moveTowardCenter();}
 }
 
+
 function draw() {
   cc.clearRect(0,0,canvasWidth,canvasHeight);
 
   for (x=0; x < (canvasWidth+global.offset.x)/global.scale.x; x++){
     for (y=0; y < (canvasHeight+global.offset.y)/global.scale.y; y++){
-      cellDraw(x, y);
+      drawCell(x, y);
     }
   }
 
+  context.clearRect(0,0,canvasWidth,canvasHeight);
+  context.drawImage(buffer, 0, 0, buffer.width, buffer.height, 0, 0, canvas.width, canvas.height);
   window.requestAnimationFrame(draw);
 }
 
@@ -79,35 +116,19 @@ start();
 ///////////  The Meat  /////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function cellDraw(x, y) {
-
-  offset = {x:(global.scale.x * x - global.offset.x),
-            y:(global.scale.y * y - global.offset.y)};
+function drawCell(x, y) {
+  offset = { x: (global.scale.x * x - global.offset.x), y: (global.scale.y * y - global.offset.y) };
   cell = world(global.pos.x+x,global.pos.y+y);
+  if (!cell) {return;} // someday I will have a clever idea to handle this case
   if (!cell.active){
-    cc.fillStyle = SimpleColor.black;
-    cc.fillRect(offset.x + drawMargin,
-              offset.y + drawMargin,
-              global.scale.x - drawMargin,
-              global.scale.y - drawMargin)
+    drawEmptyCell(offset);
   } else {
-    cc.fillStyle = Object.values(SimpleColor)[cell.number];
-    cc.fillRect(offset.x + drawMargin,
-              offset.y + drawMargin,
-              global.scale.x - drawMargin,
-              global.scale.y - drawMargin)
+    drawCellColor(offset,cell.number);
     if (settings.textOn){
       if (settings.pipsInstead) {
         drawPips(offset,cell.number);
       } else {
-        cc.fillStyle = SimpleColor.black;
-        cc.font = global.scale.y + "px Georgia";
-        cc.strokeStyle = SimpleColor.white;
-        text = cell.number;
-        width = cc.measureText(text).width;
-        cc.lineWidth = global.scale.y/40; // experimental, feels right
-        cc.strokeText(text, offset.x + (global.scale.x - width)/2, offset.y + global.scale.y*7/9);
-        cc.fillText(text, offset.x + (global.scale.x - width)/2, offset.y + global.scale.y*7/9);
+        drawText(offset,cell.number);
       }
     }
   }
@@ -159,7 +180,15 @@ function correctOffsetAndPos() {
 }
 
 
-function drawPips(offset,number) {
+function drawCellColor(offset, number) {
+  cc.fillStyle = Object.values(SimpleColor)[number];
+  cc.fillRect(offset.x + drawMargin,
+              offset.y + drawMargin,
+              global.scale.x - drawMargin,
+              global.scale.y - drawMargin)
+}
+
+function drawPips(offset, number) {
   pipUnit = { x:global.scale.x / 10, y:global.scale.y / 10 };
   pipRadius = Math.min(pipUnit.x,pipUnit.y);
   cc.strokeStyle = SimpleColor.black;
@@ -177,13 +206,33 @@ function drawPips(offset,number) {
       case 6: cc.arc(offset.x + 2*pipUnit.x, offset.y + 8*pipUnit.y, pipRadius, 0, 2 * Math.PI); break;
       case 7: cc.arc(offset.x + 5*pipUnit.x, offset.y + 8*pipUnit.y, pipRadius, 0, 2 * Math.PI); break;
       case 8: cc.arc(offset.x + 8*pipUnit.x, offset.y + 8*pipUnit.y, pipRadius, 0, 2 * Math.PI); break;
-      default: console.log("something has gone wrong!!!!")
+      default: console.log("~~Too many pips, too many pips~~"); drawText(offset,number);
     }
     cc.stroke();
     cc.fill();
     cc.closePath();
   }
 }
+
+function drawText(offset, number) {
+  cc.fillStyle = SimpleColor.black;
+  cc.font = global.scale.y + "px Georgia";
+  cc.strokeStyle = SimpleColor.white;
+  text = number;
+  width = cc.measureText(text).width;
+  cc.lineWidth = global.scale.y/40; // experimental, feels right
+  cc.strokeText(text, offset.x + (global.scale.x - width)/2, offset.y + global.scale.y*7/9);
+  cc.fillText(text, offset.x + (global.scale.x - width)/2, offset.y + global.scale.y*7/9);
+}
+
+function drawEmptyCell(offset) {
+  cc.fillStyle = SimpleColor.black;
+  cc.fillRect(offset.x + drawMargin,
+              offset.y + drawMargin,
+              global.scale.x - drawMargin,
+              global.scale.y - drawMargin)
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 ///////////  SETTING SETTINGS  /////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -194,7 +243,6 @@ function textToggle() {
 function pipToggle() {
   settings.pipsInstead = !settings.pipsInstead;
 }
-
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -220,12 +268,12 @@ function onClick(){
   handleClick({ x:event.offsetX, y:event.offsetY});
 };
 
-// this will need to be broken out once there are other things to click on
+// this will need to be broken out further once there are other things to click on
 function handleClick(pos){
   globalPos = findGlobal(pos);
   setActive(globalPos);
 
-  global.lastClick = globalPos; // dup just in case
+  global.lastClick = globalPos;
   global.moving = true;
 }
 
